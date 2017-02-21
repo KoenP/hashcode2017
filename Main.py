5abfe00afe21ff7282d490279f24ccbb34335954
Koen Pauwels <koenpauwels2@gmail.com>
2017-02-21 21:34:06 +0100

    parser

--- Main.py
+++ Main.py
@@ -9,7 +9,17 @@ class Pizza:
 
 # Returns a pizza
 def parse(filename):
-    pass
+    f = open(filename)
+    lines = f.readlines()
+
+    [rows, columns, L, H] \
+        = map(int, lines[0].split())
+
+    layout = [[] for _ in range(rows)]
+    for (i, line) in enumerate(lines[1:]):
+        layout[i] = list(map(lambda x:x, line[:-1]))
+
+    return Pizza(rows, columns, L, H, layout)
 
 # format slices: [((r_11,c_11), (r_12,c_12)), ((r_21,c_21), (r_22,c_22)), ...]
 # returns the points for the slices
