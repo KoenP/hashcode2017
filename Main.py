43107e6432aa9a71eb4b82c87f7ebd11b9221f6d
Koen Pauwels <koenpauwels2@gmail.com>
2017-02-21 21:23:29 +0100

    main

---
+++ Main.py
@@ -0,0 +1,23 @@
+class Pizza:
+
+    def __init__(row, column, L, H, layout):
+        self.row = row
+        self.column = column
+        self.L = L
+        self.H = H
+        self.grid = layout
+
+# Returns a pizza
+def parse(filename):
+    pass
+
+# format slices: [((r_11,c_11), (r_12,c_12)), ((r_21,c_21), (r_22,c_22)), ...]
+# returns the points for the slices
+def evaluateSlices(pizza, slices):
+    pass
+
+# various algs
+
+# Encodes slices to output format (same format as above)
+def slicesToOutput(slices):
+    pass
